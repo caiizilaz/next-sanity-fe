cf6180500aa8979e380a97c3dcdffb4a2325af60
caiizilaz <caiizilaz@gmail.com>
2022-07-26 15:24:47 +0700

    add env var

--- client.js
+++ client.js
@@ -1,8 +1,8 @@
 import sanityClient from '@sanity/client'
 
 export default sanityClient({
-  projectId: 'egf30eap', // you can find this in sanity.json
-  dataset: 'production',
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID, // you can find this in sanity.json
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
   useCdn: true, // `false` if you want to ensure fresh data
-  apiVersion: '2022-07-25'
+  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION
 })
