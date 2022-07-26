12f42af9517b58f3a0ee65bcbedd73413cf07240
caiizilaz <caiizilaz@gmail.com>
2022-07-26 13:28:04 +0700

    add apiversion

--- client.js
+++ client.js
@@ -3,5 +3,6 @@ import sanityClient from '@sanity/client'
 export default sanityClient({
   projectId: 'egf30eap', // you can find this in sanity.json
   dataset: 'production',
-  useCdn: true // `false` if you want to ensure fresh data
+  useCdn: true, // `false` if you want to ensure fresh data
+  apiVersion: '2022-07-25'
 })
